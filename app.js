141496de1ab75f6970b9a8e7f714eb2d8aa89e83
Robyn <oddert@gmail.com>
2018-09-11 17:09:17 +0100

    Added main page in react and initial build setup

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
-const express = require('express'),
-      app = express(),
-      bodyParser = require('body-parser'),
-      cookieParser = require('cookie-parser');
+const express       = require('express'),
+      app           = express(),
+      bodyParser    = require('body-parser'),
+      cookieParser  = require('cookie-parser'),
+      path          = require('path');
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(__dirname + '/client/build')))
 
 app.get('/', (req, res) => res.send('Server res ok'))
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'))
+})
 
 const PORT = process.env.PORT || 3000
 var server = app.listen(
